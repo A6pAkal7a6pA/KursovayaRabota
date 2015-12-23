19118c826636e5724516bd84d368eb75982af4b8
A6pAkal7a6pA <Maxkyl3d@mail.ru>
2015-12-23 21:41:39 +0200

    SnakeGame_23.12

---
+++ src/GameObjects/snakegame/objects/Apple.java
@@ -0,0 +1,23 @@
+package GameObjects.snakegame.objects;
+
+
+import gm.snakegame.SnakeGame;
+
+public class Apple {
+    public int posX;
+    public int posY;
+
+    SnakeGame main;
+
+    public Apple(int startX, int startY){
+        posX = startX;
+        posY = startY;
+    }
+
+    public void  setRandomPosition() {
+        posX = (int) (Math.random()*main.WIDTH);
+        posY = (int) (Math.random()*main.WIDTH);
+
+
+    }
+}
